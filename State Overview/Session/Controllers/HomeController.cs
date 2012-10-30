ef358c41e540272ed84140a4aebafce6cc225bef
jixer <me@bloggedbychris.com>
2012-10-30 15:44:34 -0700

    Added state examples

---
+++ State Overview/Session/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            object val = Session["val"];
+            return View("Index", val);
+        }
+
+        [HttpPost]
+        public ActionResult Put(string value)
+        {
+            Session["val"] = value;
+            Response.Cookies.Add(new HttpCookie("val", value));
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Cookie()
+        {
+            var cookie = Request.Cookies["val"];
+            return View((object)cookie.Value);
+        }
+
+        public ActionResult Local()
+        {
+            return View();
+        }
+    }
+}
